a9275be4b1c9b0b815b649ef8870580b9ce25e7b
sunsunyoung2jjang <jyp4703@nate.com>
2021-12-03 14:53:49 +0900

    3 commit

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 import boto3
 
-ec2 = boto3.resource('ec2')
+#ec2 = boto3.resource('ec2')
+client = boto3.client('ec2')
+
+
+
+def main():
+    print('--------------------------------------')
+    print('1. list instance     2. available zones ')
+    print('-------------------------------------')
+
+def start_instance():
+    output = input('Enter instance id: ')
+    response = client.start_instances(InstanceIds=[output])
+    print(f'Starting .... {output}')
+    print(f'Successfully started instance {output}')
+
+
+
+while(True):
+    Num = int(input('Enter an integer: '))
+    if Num == 3:
+        start_instance()
+    else:
+        print('error')
